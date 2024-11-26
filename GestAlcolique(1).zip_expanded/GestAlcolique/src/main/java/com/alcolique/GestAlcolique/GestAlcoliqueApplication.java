c7540acebfb5bb339725bbebe77d89201f5a7243
failonirina <you@example.com>
2024-11-26 05:01:39 +0100

    initialisation Git

---
+++ GestAlcolique(1).zip_expanded/GestAlcolique/src/main/java/com/alcolique/GestAlcolique/GestAlcoliqueApplication.java
@@ -0,0 +1,13 @@
+package com.alcolique.GestAlcolique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestAlcoliqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestAlcoliqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alcolique/GestAlcolique/GestAlcoliqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcolique.GestAlcolique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestAlcoliqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
